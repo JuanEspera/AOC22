8fe0240edaa48f7f89c244a06efdb7a8bbfb5fa8
JuanEspera <speragianmarco@gmail.com>
2022-12-06 13:02:10 +0100

    Day4

--- Day4/Day4.jl
+++ Day4/Day4.jl
@@ -4,7 +4,9 @@
 file_name = "Day4.txt"
 # file_name = "Day4test.txt"
 #
-value  = 0
+value_1  = 0
+value_2  = 0
+
 open( file_name, "r") do f
 
     while ! eof(f)
@@ -13,38 +15,40 @@ open( file_name, "r") do f
         # println( c)
         elf_1 = split(c[1], '-')
         elf_2 = split(c[2], '-')
-        c = [ elf_1[i] for i in Int(1):length(elf_1)]
-        # when one is fully contained in the other
-        if elf_1[1] <= elf_2[1]
-            if elf_1[2] >= elf_2[2]
-                global value = value + 1
-            end
-        elseif elf_2[1] <= elf_1[1]
-            if elf_2[2] >= elf_1[2]
-                global value = value + 1
-            end
-        elseif elf_1[1] <= elf_2[2]
-            if elf_1[1] >= elf_1[2]
-                global value = value + 1
-            end
-        elseif elf_2[1] <= elf_1[2]
-            if elf_2[1] >= elf_2[2]
-                global value = value + 1
-            end
-        end
-        #
-        # if elf_1[1] <= elf_2[2]
-        #     if elf_1[2] >= elf_2[2]
-        #     println(elf_1, " ", elf_2)
-        #     println( " ok funziona")
-        #     end
-        # end
+        elf_1 = [ parse(Float64,elf_1[i]) for i in 1:2 ]
+        elf_2 = [ parse(Float64,elf_2[i]) for i in 1:2 ]
 
+        # c = [ elf_1[i] for i in Int(1):length(elf_1)]
+        # when one is fully contained in the other
+        if elf_1[1] <= elf_2[1] && elf_1[2] >= elf_2[2]
+            # println("First " elf_2, " ", elf_1)
+            global value_1 = value_1 + 1
+        elseif elf_2[1] <= elf_1[1] && elf_2[2] >= elf_1[2]
+            # println( "Second ", elf_1, " ", elf_2)
+            global value_1 = value_1 + 1
+        end
+
+        if elf_1[1] <= elf_2[1] && elf_1[2] >= elf_2[2]
+            global value_2 = value_2 + 1
+        elseif elf_2[1] <= elf_1[1] && elf_2[2] >= elf_1[2]
+            global value_2 = value_2 + 1
+        elseif elf_1[2] >= elf_2[1] && elf_1[2] <= elf_2[2]
+            global value_2 = value_2 + 1
+        elseif elf_1[1] <= elf_2[2] && elf_1[1] >= elf_2[1]
+            global value_2 = value_2 + 1
+        elseif elf_2[2] >= elf_1[1] && elf_2[2] <= elf_1[2]
+            global value_2 = value_2 + 1
+        elseif elf_2[1] <= elf_1[2] && elf_2[1] >= elf_1[1]
+            global value_2 = value_2 + 1
+        end
     end
 end
 
-println( value)
+println( "First rule ", value_1)
+println( "Second rule ", value_2)
 
-
+# test =
+# b    = parse(Float64, test)
+# println(b, " ", typeof(b))
 
 # https://docs.julialang.org/en/v1/manual/conversion-and-promotion/
